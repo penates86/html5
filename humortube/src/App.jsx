50e86d70fe942d0af9bb0a55672190d90a15266f
penates86 <penates86@gmail.com>
2025-06-26 13:30:54 +0900

    2

--- humortube/src/App.jsx
+++ humortube/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return(
-  <div>
+  <div className="center_div">
     <h3>유머튜브</h3>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/niPFVYnDJ44?si=kKhFZNGsiv7mo5SL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   
