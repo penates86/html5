eab6c21a5160eca1eff09bc804c9320ef808f995
penates86 <penates86@gmail.com>
2025-06-26 11:28:36 +0900

    올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return(
+  <div>
+    <h3>유머튜브</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/niPFVYnDJ44?si=kKhFZNGsiv7mo5SL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/Rj50QIygDK4?si=4Ax0g24SHoPerNBQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/S66uUQ2eyDc?si=QWHK5zLFwawE39Rs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     <h1>사진관련</h1>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/ubH7vI2rPR4?si=ZzuQJ08o5mBj1uOh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+  )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,4 @@
+let a=1
+const b=2
+const c=a+b
+console.log(c)
